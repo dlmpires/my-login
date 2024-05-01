9b14cebec3afd8d92a9cd799e732ab4168b094c7
Diogo Pires <diogo.pires2211@gmail.com>
2024-05-01 02:43:00 +0100

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 import express from 'express';
 import db from './db.js';
+import passport from 'passport';
+import bodyParser from 'body-parser';
 
 const app = express()
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.send('ola')
 })
 
 app.get('/users', (req, res) => {
-    db.query('SELECT * FROM users', (error, results) => {
-      if (error) {
+    db.query('SELECT * FROM users', (err, results) => {
+      if (err) {
         res.status(500).send('Error fetching users');
         return;
       }
       res.json(results);
     });
-  });  
+  });
 
 app.listen(6969, () => {
     console.log(`App listening on http://localhost:${6969}`);
